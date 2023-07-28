48160a25dddea55933bab05a3189ba9d57e1dadb
siri0314 <37826044+siri0314@users.noreply.github.com>
2023-07-28 15:39:23 -0400

    Update ResultGraph.jsx

--- quiz-game/src/Components/Faculty/ResultGraph.jsx
+++ quiz-game/src/Components/Faculty/ResultGraph.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { CChart } from '@coreui/react-chartjs'
 import { useEffect, useState } from 'react'
 
+/**
+ * 
+ * @param {*} arr an array of elements for which you want to group duplicates and count their occurrences.
+ * @param {*} N  a number that defines the range of elements to count occurrences.
+ * @returns an object result, where each key represents a unique element from the arr, and the corresponding value represents the count of occurrences of that element in the arr.
+ * 
+ */
 function groupByDuplicates(arr, N) {
   const result = {};
   console.log(arr,N)
@@ -19,7 +26,14 @@ function groupByDuplicates(arr, N) {
   console.log(result)
   return result;
 }
-
+/**
+ * 
+ * @param {*} param0 This prop represents an array of data containing quiz attempts with their respective scores. Each element in the array is expected to be an object with a data property, which holds the score for that attempt
+ * @param{quiz} This prop represents an object containing details of the quiz. It should have a max_mark property representing the maximum possible score for the quiz.
+ * 
+ * @returns CChart component
+ * 
+ */
 function ResultGraph({ data, quiz }) {
   const [reqData,setReqData] = useState([])
   useEffect(()=>{
