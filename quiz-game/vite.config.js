96e805a2c9929160d41ab46341dc2c70129214cc
Siri <sirisirigiri@gmail.com>
2023-07-30 17:56:33 -0400

    Code Coverage

--- quiz-game/vite.config.js
+++ quiz-game/vite.config.js
@@ -1,7 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test:{
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary','json'],
+    },
+    globals : true,
+    environment : 'jsdom',
+    css : 'ture'
+  }
 })
